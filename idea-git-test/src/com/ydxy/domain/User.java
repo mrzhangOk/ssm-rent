efadc9445f74863c570dacc4d1f460aaca905ec5
mrzhangOk <66662408+mrzhangOk@users.noreply.github.com>
2020-06-10 16:59:32 +0800

    test5
    test5

--- idea-git-test/src/com/ydxy/domain/User.java
+++ idea-git-test/src/com/ydxy/domain/User.java
@@ -16,5 +16,8 @@ public class User {
      public static void test4(){
         System.out.println("xxxxx");
     }
+     public static void test5(){
+        System.out.println("xxxxx");
+    }
 
 }
